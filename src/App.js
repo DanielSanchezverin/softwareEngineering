a27fc2846ee11365ac5e2e483d4b2c9169d77eb4
DanielSanchezverin <danielsanchezverin@gmail.com>
2022-10-24 17:42:20 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Home from "./components/Home"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
+import "./index.css";
+
 
 function App() {
     return (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
-import { AppBar, Toolbar } from '@mui/material'
+import { AppBar, Toolbar, Button, Grid, Container } from '@mui/material'
 import React from 'react'
 import logo from '../static/images/Logo.png'
+import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <AppBar position ="static">
-      <Toolbar>
-        <img src={logo} alt="logo" edge="start"/>
-      </Toolbar>
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <img src={logo}></img>
+        </Toolbar>
+      </Container>
     </AppBar>
   )
 }
