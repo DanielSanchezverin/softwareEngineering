729341786c8cf62f55c24816882828af89e7ca0b
DanielSanchezverin <danielsanchezverin@gmail.com>
2022-10-28 00:19:48 -0500

    new contact page

--- src/components/ContactLayout.js
+++ src/components/ContactLayout.js
@@ -1,4 +1,4 @@
-import { Grid, Typography, Button } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
 import React from 'react'
 
 const ContactLayout = () => {
@@ -8,53 +8,29 @@ const ContactLayout = () => {
       backgroundColor: "#000000",
       height: "100%",
       border: "1px solid #FF7A00"}}>
-        <Grid container style={{ height: "100%",
-        width: "100%",
-        paddingLeft:"100px",
-        paddingTop: "25px",
-        paddingBottom: "25px"}}>
-          <Grid item xs={6} style={{ height: "100%"}}>
-            <img src="/images/SanitizacionImage.png" alt="Sanitizacion"></img>
+      <Grid container style={{ height: "100%",
+      width: "100%",
+      paddingLeft:"100px",
+      paddingTop: "25px",
+      paddingBottom: "25px"}}>
+        <Grid item xs={8}>
+          <img src="/images/ContactImage.png" alt="Contacto" style={{ height: "100%", width: "100%"}}></img>
+        </Grid>
+        <Grid item xs={4} style={{ height: "100%" }}>
+          <Grid container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          style={{ minHeight: '100vh' }}>
+            <Typography variant="h2" fontWeight="700" color="white" align="center">
+            Contactanos!
+            </Typography>
+            <Typography variant="h6" fontWeight="500" color="primary" align="center">
+              Numero: 844-XXX-XXXX <br/> Correo electronico: XXXX@plagasysanitizacion.tk
+            </Typography>
           </Grid>
-          <Grid item xs={6} style={{ height: "100%" }}>
-            <Grid container
-            spacing={0}
-            direction="column"
-            alignItems="center"
-            justifyContent="center"
-            style={{ minHeight: '100vh' }}>
-              <Typography variant="h2" fontWeight="700" color="white" align="center">
-                Necesitas ayuda cotizando?
-              </Typography>
-              <Typography variant="h6" fontWeight="500" color="primary">
-                Con este servicio podras agendar una cita para hacer tu cotizacion!
-              </Typography>
-              <div>
-                <Button variant="contained" color="secondary"size="large" style={{marginTop:"20px"}}>Cotizar</Button>
-              </div>
-            </Grid>
-          </Grid>
-          <Grid item xs={6} style={{ height: "100%"}}>
-            <img src="/images/ServiceImage.png" alt="Servicios"></img>
-          </Grid>
-          <Grid item xs={6} style={{ height: "100%" }}>
-            <Grid container
-            spacing={0}
-            direction="column"
-            alignItems="center"
-            justifyContent="center"
-            style={{ minHeight: '100vh' }}>
-              <Typography variant="h2" fontWeight="700" color="white" align="center">
-                Control de plagas <br></br> y <br></br> Sanitizacion
-              </Typography>
-              <Typography variant="h6" fontWeight="500" color="primary">
-                Aqui podras seleccionar cualquiera de nuestros servicios!
-              </Typography>
-              <div>
-                <Button variant="contained" color="secondary"size="large" style={{marginTop:"20px"}}>Seleccionar Servicio</Button>
-              </div>
-            </Grid>
-          </Grid>
+        </Grid>
       </Grid>
     </div>
   );
